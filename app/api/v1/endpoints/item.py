d56fb5530ce193da651526e5ec34cc4460d2f85a
hdkim <eastshine94@naver.com>
2026-01-26 13:02:09 +0900

    feat: 요청 본문

--- app/api/v1/endpoints/item.py
+++ app/api/v1/endpoints/item.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 @router.get("/{item_id}")
 async def read_item(item_id: str, needy: str, q: str | None = None, short: bool = False, ):
@@ -11,3 +19,19 @@ async def read_item(item_id: str, needy: str, q: str | None = None, short: bool 
         item.update({"description": "This is an amazing item that has a long description"})
 
     return item
+
+@router.post("/")
+async def create_item(item:Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@router.put("/{item_id}")
+async def update_item(item_id:int, item: Item, q: str |None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q is not None:
+        result.update({"q":q})
+    return result
