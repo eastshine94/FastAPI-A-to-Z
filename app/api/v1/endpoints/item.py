f4f8611568c11a7a353d55109fbea2c07150c9fd
hdkim <eastshine94@naver.com>
2026-01-30 16:58:43 +0900

    요청 예제 데이터 선언

--- app/api/v1/endpoints/item.py
+++ app/api/v1/endpoints/item.py
@@ -16,7 +16,9 @@ class Item(BaseModel):
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None
     tags: set[str] = [] # set으로 지정 시 고유한 항목들의 집합으로 출력됨
-    images: list[Image] | None = None
+    images: list[Image] | None = None   
+    
+
 
 class Offer(BaseModel):
     name: str
@@ -27,6 +29,16 @@ class Offer(BaseModel):
 class User(BaseModel):
     username: str
     full_name: str | None = None
+    model_config = {
+       "json_schema_extra":{
+            "examples":[
+                {
+                    "username": "Kim",
+                    "full_name": "KillDong"
+                }
+            ]
+       }
+    }
 
     
 data = {
@@ -82,7 +94,36 @@ async def read_item(item_id: Annotated[int,Path(title="The ID of the item to get
     return item
 
 @router.post("/")
-async def create_item(item:Annotated[Item, Body(embed=True)]):
+async def create_item(item:Annotated[Item, Body(
+        # embed=True,
+        openapi_examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    )]):
     item_dict = item.model_dump()
     if item.tax is not None:
         price_with_tax = item.price + item.tax
@@ -99,7 +140,28 @@ async def create_multiple_images(images: list[Image]):
 @router.put("/{item_id}")
 async def update_item(
     item_id: Annotated[int, Path(title="The ID of the item to get" ,ge=0, le=1000)],
-    item: Item, user: User, 
+    item: Annotated[
+        Item,
+        Body(
+            examples=[
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+                {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+                {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            ],
+        ),
+    ], 
+    user: User, 
     importance: Annotated[int, Body(gt=0)],
     q: str | None = None):
 
