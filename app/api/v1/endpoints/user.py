044ca2de3e6b0300d20c1f53aa983df95812ab5f
hdkim <eastshine94@naver.com>
2026-02-02 13:43:00 +0900

    다중 모델

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -15,6 +15,25 @@ class UserIn(BaseUser):
     password: str
 
 
+class UserOut(BaseUser):
+    pass
+
+
+class UserInDB(BaseUser):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.model_dump(), hashed_password=hashed_password)
+    print("User saved! ..not really", user_in_db)
+    return user_in_db
+
+
 @router.get("/{user_id}/items/{item_id}")
 async def read_user_item(
     user_id: int, item_id: str, q: str | None = None, short: bool = False
@@ -29,6 +48,7 @@ async def read_user_item(
     return item
 
 
-@router.post("/", response_model=BaseUser, response_model_include=["username"])
-async def create_user(user: UserIn):
-    return user
+@router.post("/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
