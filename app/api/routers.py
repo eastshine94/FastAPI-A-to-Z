0b05bda4a318c3afe7e543ed48705766f284ff87
hdkim <eastshine94@naver.com>
2026-02-03 10:58:45 +0900

    파일 요청

--- app/api/routers.py
+++ app/api/routers.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 api_router.include_router(item.router, prefix="/items", tags=["items"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(model.router, prefix="/models", tags=["models"])
-api_router.include_router(file.router, prefix="/files", tags=["files"])
+api_router.include_router(file.router, tags=["files"])
 api_router.include_router(
     index_weights.router, prefix="/index-weights", tags=["index weights"]
 )
--- app/api/v1/endpoints/file.py
+++ app/api/v1/endpoints/file.py
@@ -1,8 +1,47 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, File, UploadFile
 
 router = APIRouter()
 
 
-@router.get("/{file_path:path}")
+@router.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@router.post("/files/")
+async def create_file(
+    file: Annotated[bytes | None, File(description="A file read as bytes")] = None,
+):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
+
+
+@router.post("/files/")
+async def create_files(
+    files: Annotated[list[bytes], File(description="Multiple files as bytes")],
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@router.post("/uploadfile/")
+async def create_upload_file(
+    file: Annotated[
+        UploadFile | None, File(description="A file read as UploadFile")
+    ] = None,
+):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
+
+
+@router.post("/uploadfiles/")
+async def create_upload_files(
+    files: Annotated[
+        list[UploadFile], File(description="Multiple files as UploadFile")
+    ],
+):
+    return {"filenames": [file.filename for file in files]}
